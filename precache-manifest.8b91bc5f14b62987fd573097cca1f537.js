6641566b5ae81b540ac0566f63cdd062e56ecdd5
Arthur Carneiro <arthur.carneirontk@gmail.com>
2020-06-21 15:46:26 -0300

    Updates

---
+++ precache-manifest.8b91bc5f14b62987fd573097cca1f537.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc2758f0ad74f0e58fd9f301b82ceb7",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "368dea2aa26064e8a94d",
+    "url": "/frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260d135e8d8b717bf145",
+    "url": "/frontend/static/js/2.9598ca2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.9598ca2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368dea2aa26064e8a94d",
+    "url": "/frontend/static/js/main.b26c3854.chunk.js"
+  },
+  {
+    "revision": "01074640edd016698ea9",
+    "url": "/frontend/static/js/runtime-main.60624324.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend/app/precache-manifest.d7269ffd130179eb78ab342f2b68467b.js"
+  "/frontend/precache-manifest.8b91bc5f14b62987fd573097cca1f537.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
